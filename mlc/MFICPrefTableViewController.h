216a4704ec350524a8d11056de752be00174a1ae
Kevin Bradley <kevin.w.bradley@me.com>
2019-07-08 04:06:14 -0700

    oops

---
+++ mlc/MFICPrefTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  MFICPrefTableViewController.h
+//  pubc
+//
+//  Created by Kevin Bradley on 6/21/19.
+//  Copyright © 2019 nito llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MFICPrefTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
